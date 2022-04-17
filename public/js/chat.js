b722027f7d1edab5ce22327afce670581e95acb0
unknown <kartikgarg938@gmail.com>
2022-04-17 16:08:37 +0530

    socket.io room

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,6 +15,9 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
 
+//option
+const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix:true})
+
 socket.on('message', (message)=>{
     console.log(message)
     const html = Mustache.render(messageTemplate, {
@@ -73,3 +76,5 @@ $sendLocation.addEventListener('click', ()=>{
     })
 
 })
+
+socket.on('join', {username, room})
--- src/index.js
+++ src/index.js
@@ -27,8 +27,14 @@ io.on('connection', (socket)=>{
 
     //send welcome to new connections, so have to use socket, because already connected ones should not 
     //recieve any new messages
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined'))
+    
+    socket.on('join', ({username, room})=>{
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    })
+
     socket.on('sendMessage', (message, callback)=>{
         const filter = new Filter()
 
@@ -50,4 +56,6 @@ io.on('connection', (socket)=>{
         socket.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.latitude},${location.longitude}`))
         callback()
     })
+
+    
 })
